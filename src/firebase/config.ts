5d0113bc2107c6fbc8d2b76879004725f9d18bbd
amir soleymani <moslem1070@gmail.com>
2025-12-19 13:00:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3034919778-212b6",
+  "appId": "1:962181466506:web:8ce3b744fd61389e896073",
+  "apiKey": "AIzaSyBH61O_asYdFUWMQ5WycaNNppXjG-Rz2JI",
+  "authDomain": "studio-3034919778-212b6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "962181466506"
+};
