32d9b11048b34647f0142e2523873ad68c6787a4
amir soleymani <moslem1070@gmail.com>
2025-12-21 15:49:37 +0000

    میخوام فونتش کلمه باشه

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Kalameh:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased bg-gradient-to-br from-[#302851] to-[#1A162E] min-h-screen">
         <FirebaseClientProvider>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['Kalameh', 'sans-serif'],
+        headline: ['Kalameh', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
